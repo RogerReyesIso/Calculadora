2d13448a538307ff78ea4e3f67f3d06c61350b4b
roger <roger@LAPTOP-7E6R576I>
2018-09-26 16:21:21 +0200

    Commit introduir dos nombres

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,9 +1,20 @@
 package calculadora;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         
+        int num1 = 0;
+        int num2 = 0;
+        
+        System.out.println("Introdueix un nombre: ");
+        num1 = in.nextInt();
+        
+        System.out.println("Introdueix un altre nombre: ");
+        num2 = in.nextInt();
     }
     
 }
